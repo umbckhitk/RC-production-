5153b4a174b1ee8c8a2cc72168ae7c80c7a54957
umbckhitk <105472386+umbckhitk@users.noreply.github.com>
2023-12-12 12:37:35 -0500

    Add files via upload

---
+++ LoginApp/src/main/java/com/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bean.LoginBean;
+
+@Service
+public class LoginService {
+
+public boolean validate(LoginBean bean) {
+	
+ //bean.getUserName().equals(regist));
+ 
+ return true;
+}
+
+
+}
